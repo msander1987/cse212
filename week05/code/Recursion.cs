6b59c36b159634ac8919b18847dedff91fb05cd9
Julio Martin Sander <msander1987@gmail.com>
2025-08-01 13:56:16 -0300

    stairs problem finished

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -108,6 +108,10 @@ public static class Recursion
     /// </summary>
     public static decimal CountWaysToClimb(int s, Dictionary<int, decimal>? remember = null)
     {
+        // Check if already in dictionary
+        if (remember == null){
+            remember = new Dictionary<int, decimal>();
+        }        
         // Base Cases
         if (s == 0)
             return 0;
@@ -119,9 +123,11 @@ public static class Recursion
             return 4;
 
         // TODO Start Problem 3
-
+        if (remember.ContainsKey(s))
+        return remember[s];
         // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+        decimal ways = CountWaysToClimb(s - 1, remember) + CountWaysToClimb(s - 2, remember) + CountWaysToClimb(s - 3, remember);
+        remember[s] = ways;
         return ways;
     }
 
