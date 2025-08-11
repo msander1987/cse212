f8e494f588191716804ff27eca7bb9b08c44f104
Julio Martin Sander <msander1987@gmail.com>
2025-08-11 01:52:43 -0300

    first 4 problems done

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 public class Node
 {
     public int Data { get; set; }
@@ -12,34 +13,96 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        if (value != Data)
+        {
 
-        if (value < Data)
-        {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
+            if (value < Data)
+            {
+                // Insert to the left
+                if (Left is null)
+                    Left = new Node(value);
+                else
+                    Left.Insert(value);
+            }
             else
-                Left.Insert(value);
+            {
+                // Insert to the right
+                if (Right is null)
+                    Right = new Node(value);
+                else
+                    Right.Insert(value);
+            }
         }
-        else
-        {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
-        }
+
     }
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+        {
+            return true;
+        }
+
+        if (value < Data)
+        {
+            // Check in the left
+            if (Left is null)
+            {
+                return false;
+            }
+            return Left.Contains(value);
+        }
+        else
+        {
+            //Check in the right
+            if (Right is null)
+            {
+                return false;
+            }
+            return Right.Contains(value);
+        }
+
+
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        if (Left is null && Right is null)
+        {
+            return 1;
+        }
+
+        int left = 0;
+        int right = 0;
+        if (Left is null)
+        {
+            left = 0;
+        }
+        else
+        {
+            left = Left.GetHeight();
+        }
+
+        if (Right is null)
+        {
+            right = 0;
+        }
+        else
+        {
+            right = Right.GetHeight();
+        }
+
+        int finalHeigth = left - right;
+        if (finalHeigth <= 0)
+        {
+            return right + 1;
+        }
+        else
+        {
+            return left + 1;
+        }
+
+
+
     }
 }
